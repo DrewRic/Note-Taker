23a00a8113f4b87b3fc8a31b8470a1f20a00e685
DrewRic <andrewricker1@gmail.com>
2024-09-10 18:17:19 -0400

    uuid push-forcepush

--- routes/routes.js
+++ routes/routes.js
@@ -1,19 +1,21 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-const { v4: uuidv4 } = require("uuid");
+const { v4: uuidv4 } = require('uuid');
 
 const router = express.Router();
-var notesDB = require("./db/db.json");
+var notesDB = require("../db/db.json");
+
+
 
 // Serve the index.html page
 router.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "./public/index.html"))
+  res.sendFile(path.join(__dirname, "../public/index.html"))
 );
 
 // Serve the notes.html page
 router.get("/notes", (req, res) =>
-  res.sendFile(path.join(__dirname, "./public/notes.html"))
+  res.sendFile(path.join(__dirname, "../public/notes.html"))
 );
 
 // Get all notes
@@ -26,18 +28,16 @@ router.post("/api/notes", (req, res) => {
   req.body.id = uuidv4();
   const newNote = req.body;
   notesDB.push(newNote);
-  fs.writeFileSync(path.join(__dirname, "./db/db.json"), JSON.stringify(notesDB));
+  fs.writeFileSync(path.join(__dirname, "../db/db.json"), JSON.stringify(notesDB));
   res.json(notesDB);
 });
-
-
 
 
 // Delete a note by ID
 router.delete("/api/notes/:id", (req, res) => {
   const id = req.params.id;
   notesDB = notesDB.filter((note) => note.id !== id);
-  fs.writeFileSync(path.join(__dirname, "./db/db.json"), JSON.stringify(notesDB));
+  fs.writeFileSync(path.join(__dirname, "../db/db.json"), JSON.stringify(notesDB));
   res.json(notesDB);
 });
 
